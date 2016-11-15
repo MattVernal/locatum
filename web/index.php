049a68fc50f4f2b5f57dd7c1812ec7d0651c8562
MattVernal <matthew.vernal@gmail.com>
2016-11-15 17:40:12 +1300

    first commit

--- web/index.php
+++ web/index.php
@@ -10,16 +10,44 @@ final class Index {
        
 //    System config
     public function init(){
-        //Start session
-        session_start();
-        //Load
+        //Error reporting - all errors for development
+        error_reporting(E_ALL | E_STRICT);
+        //Set Encoding
+        mb_internal_encoding('UTF-8');
+        //Set Function for exeption handling
+        set_exception_handler(array($this, 'handleException'));
+        //Load required classes
         spl_autoload_register(array($this, 'loadClass'));
-        
-        
-        
-        
+        //Start session
+        session_start();        
+    }
+    //Run application
+    public function run() {
+        $this->runPage($this->getPage());   
         
     }
+    //Exception Handler
+    public function handleException(Exception $ex){
+        $extra = array('message' => $ex->getMessage());
+        if ($ex instanceof NotFoundException){
+            header('HTTP/1.0 404 Not Found');
+            $this->runErrorPage('404', $extra);
+        } else {
+            header('HTTP/1.1 500 Internal Sever Error');
+            $this->runErrorPage('404', $extra);
+        }        
+    }
+    public function loadClass($name){
+        //array of classes to be laoded
+        $classes = array();
+        //Exception handler if class not found
+        if (!array_key_exists($name, $classes)){
+            die('Class"' . $name . '"not found.');
+        }
+        require_once $classes[$name];
+    }
+    
+    
 }
 
 
